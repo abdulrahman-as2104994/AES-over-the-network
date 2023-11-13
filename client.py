60a47ff26349500053fea234c16d3535201b2b0b
Abdulrahman Selmi <as2104994@qu.edu.qa>
2023-11-13 16:24:35 +0300

    trying key exchange

--- client.py
+++ client.py
@@ -1,13 +1,16 @@
 import socket
-from aes_128_bit import decrypt
+from aes_128_bit import decrypt, encrypt
 
-KEY = "a"
+KEY_EXCHANGE = "a"
+KEY = ""
+
 HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 1234))
 
 while True:
+    clientsocket, address = s.accept()
     full_msg = ""
     new_msg = True
     while True:
@@ -18,16 +21,17 @@ while True:
             new_msg = False
         full_msg += msg.decode("utf-8")
         if len(full_msg) - HEADERSIZE == msglen:
-            print("Encrypted message: " + full_msg[HEADERSIZE:])
-            print("Decrypted message: " + decrypt(full_msg[HEADERSIZE:], KEY))
-             
-            decrypted_msg = decrypt(full_msg[HEADERSIZE:], KEY)
-            decrypted_msg = f"{len(decrypted_msg):<{HEADERSIZE}}" + decrypted_msg
+            print("Encrypted key: " + full_msg[HEADERSIZE:])
+            KEY = decrypt(full_msg[HEADERSIZE:], KEY_EXCHANGE)
+            print("Decrypted key: " + KEY)
+            
+            msg = "Hello World!"
+            msg = f"{len(msg):<{HEADERSIZE}}" + msg
+            encrypted_msg = encrypt(KEY, msg)
+            clientsocket.send(bytes(encrypted_msg, "utf-8"))
 
-            s.send(bytes(decrypted_msg, "utf-8"))
-           
             new_msg = True 
             full_msg = ""
-            break
-    break
+            # break
+    # break
 
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import socket
-from aes_128_bit import encrypt
+from aes_128_bit import encrypt, decrypt
 
-KEY = "a"
-MSG = "Hello World!"
-CIPHER = encrypt(MSG, KEY)
+KEY_EXCHANGE = "a"
+KEY = "b"
+ENCRYPTED_KEY = encrypt(KEY_EXCHANGE, KEY)
 
 HEADERSIZE = 10
 
@@ -14,7 +14,7 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    msg = CIPHER
+    msg = ENCRYPTED_KEY
     msg = f"{len(msg):<{HEADERSIZE}}" + msg
     clientsocket.send(bytes(msg, "utf-8"))
 
@@ -28,10 +28,11 @@ while True:
             new_msg = False
         full_msg += msg.decode("utf-8")
         if len(full_msg) - HEADERSIZE == msglen:
-            print("Reply: " + full_msg[HEADERSIZE:])
+            print("Encrypted message: " + full_msg[HEADERSIZE:])
+            print("Decrypted message: " + decrypt(full_msg[HEADERSIZE:], KEY))
 
             new_msg = True 
             full_msg = ""
 
-            break
-    break
+            # break
+    # break
